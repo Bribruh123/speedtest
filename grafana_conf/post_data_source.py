7eb5f726789672bee65bb25d51f1cb99b4744965
Brian Hoch (brhoch) <brhoch@cisco.com>
2023-08-22 07:44:46 -0500

    grafana_conf_agent

---
+++ grafana_conf/post_data_source.py
@@ -0,0 +1,50 @@
+import requests
+import json
+
+url = "http://admin:admin@grafana:3000/api/auth/keys"
+
+payload = json.dumps({
+  "name": "apikeycurl",
+  "role": "Admin"
+})
+headers = {
+  'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload, verify=False)
+key = json.loads(response.text)['key']
+
+
+url = "http://grafana:3000/api/datasources"
+
+payload = json.dumps({
+  "id": None,
+  "name": "InfluxDB",
+  "type": "influxdb",
+  "typeName": "InfluxDB",
+  "access": "proxy",
+  "url": "http://influxdb:8086",
+  "user": "",
+  "database": "",
+  "basicAuth": False,
+  "isDefault": False,
+  "jsonData": {
+    "dbName": "LOGS",
+    "httpHeaderName1": "Authorization"
+  },
+  "readOnly": False,
+  "secureJsonData": {
+    "httpHeaderValue1": "Token veryverysecuretoken"
+  }
+})
+
+
+
+headers = {
+  'Authorization': 'Bearer {}'.format(key),
+  'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload, verify=False)
+
+print(response.text)
